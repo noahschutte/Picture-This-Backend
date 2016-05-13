ef876cc8381320c456eb8ff8013e16db18c0ddb2
Noah Schutte <noahschutte@gmail.com>
2016-05-13 15:10:13 -0400

    resolve associations between round and invites join table

--- app/models/participant_round.rb
+++ app/models/participant_round.rb
@@ -2,5 +2,5 @@ class ParticipantRound < ActiveRecord::Base
   validates :round_id, :participant_id, presence: true
 
   belongs_to :round
-  belongs_to :participant
+  belongs_to :participant, class_name: "User", foreign_key: :participant_id
 end
