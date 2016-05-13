3e9b80cf59efa1e5d62debeb4d4a74b504e691c9
Noah Schutte <noahschutte@gmail.com>
2016-05-13 12:23:23 -0400

    create many to many association between rounds and invites

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -2,5 +2,5 @@ class Invite < ActiveRecord::Base
   validates :round_id, presence: true
   validates :phone, :email, uniqueness: true
 
-  belongs_to :round
+  has_many :rounds, through: :round_invites
 end
--- app/models/round.rb
+++ app/models/round.rb
@@ -1,6 +1,9 @@
 class Round < ActiveRecord::Base
   validates :creator_id, :prompt_id, presence: true
 
-  belongs_to :creator
+  has_many :participants, through: :participant_rounds
+  has_many :photos
+  has_many :invites, through: :round_invites
+  belongs_to :creator, class_name: "User", foreign_key: :creator_id
   belongs_to :prompt
 end
