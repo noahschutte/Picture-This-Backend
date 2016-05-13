aa33091519f167bbb1bd61f31a281399a4b016ed
theomarkkuspaul <theo.markkus.paul@gmail.com>
2016-05-13 18:58:53 -0400

    Add tests for round

--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -2,24 +2,17 @@ require 'rails_helper'
 
 describe Round do
 
-  it { should belong_to :prompt}
+  # before(:each) do
+    # @round = Round.create(creator_id: @creator.id, prompt_id: @prompt.id)
+  # end
 
-  before(:each) do
-    @deck = Deck.create(name: 'Comedic')
-    @creator = User.create(first_name: 'Theo', last_name: 'Paul', email: 'a@b.c', phone: '12345', password: 'qwerty')
-    @prompt = Prompt.create(deck_id: @deck.id, body: 'Bursting')
-    @round = Round.create(creator_id: @creator.id, prompt_id: @prompt.id)
-  end
+  # it 'should have an id as a Fixnum' do
+    # expect((@round.id).class.to_s).to eq("Fixnum")
+  # end
 
-  it 'should have an id as a Fixnum' do
-    expect((@round.id).class.to_s).to eq("Fixnum")
-  end
-
-  context 'associations with Prompts' do
-    it 'should belong to a prompt' do
-      expect(@prompt.rounds).to match_array([])
-    end
-  end
-
+  it { should belong_to :prompt }
+  it { should belong_to :creator }
+  it { should have_many :photos }
+  it { should have_many :participants }
 
 end
