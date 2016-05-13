13f9c9d0b66fcacf96e0c58b8cc1c4adeceb164f
Noah Schutte <noahschutte@gmail.com>
2016-05-13 11:38:49 -0400

    Add references and associations to photos

--- db/migrate/20160513151728_create_photos.rb
+++ db/migrate/20160513151728_create_photos.rb
@@ -1,9 +1,9 @@
 class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
-      t.references :round, null: false, index: true
-      t.references :user, null: false, index: true
-      t.references :prompt, null: false, index: true
+      t.references :round, null: false, index: true, foreign_key: true
+      t.references :user, null: false, index: true, foreign_key: true
+      t.references :prompt, null: false, index: true, foreign_key: true
 
       t.timestamps null: false
     end
