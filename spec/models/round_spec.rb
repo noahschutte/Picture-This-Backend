c381b3a46291eacd5fab8dd56e465759172f577f
theomarkkuspaul <theo.markkus.paul@gmail.com>
2016-05-13 18:59:18 -0400

    Fix and cleanup test syntax

--- spec/models/round_spec.rb
+++ spec/models/round_spec.rb
@@ -2,14 +2,6 @@ require 'rails_helper'
 
 describe Round do
 
-  # before(:each) do
-    # @round = Round.create(creator_id: @creator.id, prompt_id: @prompt.id)
-  # end
-
-  # it 'should have an id as a Fixnum' do
-    # expect((@round.id).class.to_s).to eq("Fixnum")
-  # end
-
   it { should belong_to :prompt }
   it { should belong_to :creator }
   it { should have_many :photos }
