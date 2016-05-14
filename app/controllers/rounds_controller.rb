eb31eff34687ab7416af59111f5edaba48a453cd
Noah Schutte <noahschutte@gmail.com>
2016-05-14 17:19:21 -0400

    parse json response for round index route

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -12,7 +12,15 @@ class RoundsController < ApplicationController
 
       @rounds = open_rounds(@user)
       if request.xhr?
-        return "#{@rounds}"
+        # binding.pry
+        rounds = []
+        @rounds.each do |round|
+          rounds << {round_id: round.id, creator_id: round.creator_id, creator_first_name: round.creator.first_name, prompt: round.prompt.body, end_time: round.end_time}
+        end
+        # rounds = JSON.generate(@rounds)
+        # round = @rounds
+        render :json => { rounds: rounds }
+
       else
         return "bad request"
       end
