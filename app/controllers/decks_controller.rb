98eeff5d06f4a6b5052f620b5a0c17fe1f861f4d
Noah Schutte <noahschutte@gmail.com>
2016-05-14 19:02:22 -0400

    render JSON response for deck index and remove unused view file

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -6,6 +6,11 @@ class DecksController < ApplicationController
     # Remove this hard-coded value
     # Need to randomize prompt based on Deck selected
     @prompt = Prompt.first
+    decks = []
+    @decks.each do |deck|
+      decks << { id: deck.id, name: deck.name }
+    end
+    render :json => { decks: decks }
   end
 
 end
