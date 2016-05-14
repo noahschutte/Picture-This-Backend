66def4c5a2ef5778d55fe2429866b82f4e80aa26
Noah Schutte <noahschutte@gmail.com>
2016-05-14 19:13:07 -0400

    render JSON rounds show response content and remove unused view file

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -4,23 +4,25 @@ class RoundsController < ApplicationController
       @round = Round.find_by(id: params[:id])
       @prompt = @round.prompt
       @user = User.find_by(id: session[:user_id])
+
+      participants = []
+      @round.participants.each do |participant|
+        participants << participant.first_name
+      end
+      render :json => {
+        prompt: { id: @prompt.id },
+        round: { id: @round.id, end_time: @round.end_time, participants: participants }
+      }
     end
 
     def index
       @user = User.find_by(id: params[:user_id])
       @rounds = open_rounds(@user)
-      # if request.xhr?
-        # binding.pry
       rounds = []
       @rounds.each do |round|
         rounds << {round_id: round.id, creator_id: round.creator_id, creator_first_name: round.creator.first_name, prompt: round.prompt.body, end_time: round.end_time}
       end
-        # rounds = JSON.generate(@rounds)
-        # round = @rounds
       render :json => { rounds: rounds }
-      # else
-        # return "bad request"
-      # end
     end
 
     def new
