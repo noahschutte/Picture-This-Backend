8a54e690703b2c7fb1970210b97fe2bfedd37077
Noah Schutte <noahschutte@gmail.com>
2016-05-16 12:07:21 -0400

    replace theo and noah's phone number with real info

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,8 +24,8 @@ def random_prompt(prompts)
 end
 
 noah = User.create(first_name: "Noah", last_name: "Schutte", email: "noah@email.com", phone: "6023016624", password: "noah")
-theo = User.create(first_name: "Theo", last_name: "Paul", email: "theo@email.com", phone: "7654321", password: "theo")
-dan = User.create(first_name: "Dan", last_name: "Homer", email: "dan@email.com", phone: "7777777", password: "dan")
+theo = User.create(first_name: "Theo", last_name: "Paul", email: "theo@email.com", phone: "9293348660", password: "theo")
+dan = User.create(first_name: "Dan", last_name: "Homer", email: "dan@email.com", phone: "3022427822", password: "dan")
 
 Deck.create(name: "Family")
 Deck.create(name: "Friends")
