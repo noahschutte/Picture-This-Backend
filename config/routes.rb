a8ff1483f6cbe99f925f7294a2ce6ce3891d7170
Noah Schutte <noahschutte@gmail.com>
2016-05-16 15:58:38 -0400

    remove contacts controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,14 @@
 Rails.application.routes.draw do
 
   root 'sessions#new'
-
   resources :sessions, only: [:create]
+  resources :decks, only: [:index]
+  resources :photos, only: [:show, :create]
+  resources :rounds, only: [:create]
 
   resources :users, only:[:new, :create, :show] do
     resources :rounds, only: [:index]
   end
-
-  resources :decks, only: [:index]
-  resources :photos, only: [:show, :create]
-  resources :rounds, only: [:create]
 
   resources :rounds, only: [:show] do
     resources :photos, only: [:index]
