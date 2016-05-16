94fd34e3f8db1cc593592f7f4fb6ac5fa867d00c
Noah Schutte <noahschutte@gmail.com>
2016-05-16 16:45:42 -0400

    refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
-
-  def current_user
-    @user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
-
-  def logged_in?
-    current_user && session[:user_id]
-  end
-
-  def required_logged_in
-    redirect_to '/' unless logged_in?
-  end
-
 end
