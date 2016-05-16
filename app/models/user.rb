647734f45c8ea7277d23937d89f818d2eb1ad098
Noah Schutte <noahschutte@gmail.com>
2016-05-16 19:38:03 -0400

    refactor model responsibilities

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,27 @@ class User < ActiveRecord::Base
   validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i, :on => :create
 
   has_many :participant_rounds, foreign_key: :participant_id
-  # has_many :rounds, foreign_key: :creator_id ## Is this line necessary?
   has_many :rounds, through: :participant_rounds
   has_many :photos
+
+  def open_rounds
+    self.rounds.where("end_time > ?", DateTime.now)
+  end
+
+  def pending_rounds
+    self.open_rounds.select { |round| round.attr_hash unless round.has_photos?(self.id) }
+  end
+
+  def submitted_rounds
+    self.open_rounds.select { |round| round.attr_hash if round.has_photos?(self.id) }
+  end
+
+  def self.add_participants(round, participant_list)
+    round.participants << round.creator
+    participant_list.map do |name, phone|
+      invite = User.find_by(phone: phone)
+      round.participants << invite
+    end
+  end
+
 end
