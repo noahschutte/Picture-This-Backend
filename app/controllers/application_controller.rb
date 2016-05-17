bb3c6a42e4740bcdebb1de8f6af7935dc0eae8f4
Noah Schutte <noahschutte@gmail.com>
2016-05-16 21:37:16 -0400

    remov strong params from user create and refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
-
-  after_action :set_access_control_headers
-
-  def set_access_control_headers
-    headers['Access-Control-Allow-Origin'] = "*"
-    headers['Access-Control-Request-Method'] = %w{GET POST OPTIONS}.join(",")
-  end
 end
